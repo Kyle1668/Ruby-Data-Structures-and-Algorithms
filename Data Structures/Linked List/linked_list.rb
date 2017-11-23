37124e98f54888656b1c2afc9e3b5850da68a521
Kyle1668 <kyledevinobrien1@gmail.com>
2017-11-22 19:11:34 -0800

    prog

--- Data Structures/Linked List/linked_list.rb
+++ Data Structures/Linked List/linked_list.rb
@@ -1,10 +1,13 @@
 class LinkedList
 
   class Node
-
+    
     @data
     @prev_node
     @next_node
+
+    # attr_accessor :next_node, :prev_node, :data
+    # attr_reader :next_node, prev_node, data
 
     def initialize(in_data)
       @data = in_data
@@ -14,17 +17,17 @@ class LinkedList
 
   end
 
-  @size
-  @head_node
-  @tail_node
-  @current_node
+  @size = 0
+  @head_node = nil
+  @tail_node = nil
+  @current_node = nil
 
-  def initialize
-    @size = 0
-    @head_node = nil
-    @tail_node = nil
-    @current_node = nil
-  end
+  # def initialize
+  #   @size = 0
+  #   @head_node = nil
+  #   @tail_node = nil
+  #   @current_node = nil
+  # end
 
   def add_first_node(new_node)
     @head_node = @tail_node = @current_node = new_node
@@ -39,13 +42,29 @@ class LinkedList
     elsif @current_node == @tail_node
       new_node.prev_node = @head_node
       @head_node.next_node = new_node
-
+      @tail_node = new_node
+      @current_node = @tail_node
     else
-
+      new_node.prev_node = @current_node
+      new_node.next_node = @current_node.next_node
+      @current_node.next_node = new_node
+      @current_node = new_node
     end
 
     @size += 1
 
+  end
+
+  def print_list
+    if @size > 0
+      iterator = @head_node
+
+      until iterator.next_node == nil do
+        print iterator.data
+        iterator = iterator.next_node
+      end
+
+    end
   end
 
 end
--- Data Structures/Linked List/test.rb
+++ Data Structures/Linked List/test.rb
@@ -1,4 +1,20 @@
+require_relative 'linked_list'
+
+
 def main
   my_list = LinkedList.new
 
+  print "before loop"
+
+  for num in 1..10
+    print num
+    my_list.add(num)
+  end
+
+  print "after loop"
+
+  my_list.print_list
+
 end
+
+main
