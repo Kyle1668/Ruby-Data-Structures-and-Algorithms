94998170e90024461b2d0b0aa20433fe0eb63e15
Kyle1668 <kyledevinobrien1@gmail.com>
2017-11-25 08:42:18 -0800

    Started on Stack

---
+++ Data Structures/Stack/stack.rb
@@ -0,0 +1,25 @@
+class Stack
+
+  class Node
+
+    attr_accessor :data, :prev_node, :next_node
+    attr_writer :data, :prev_node, :next_node
+
+    def initialize(in_data)
+      @data = in_data
+      @prev_node = nil
+      @next_node = nil
+    end
+
+  end
+
+  def initialize
+    @size = 0
+    @head_node = nil
+    @tail_node = nil
+    @current_node = nil
+  end
+
+
+
+end
